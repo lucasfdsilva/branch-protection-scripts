413bc46ec4d1abc6ac6886a958a8629be109f687
Lucas da Silva <lucas.silva00@outlook.ie>
2023-02-28 14:04:00 +0000

    refactored to make it easier to update

--- remove_dir_from_all_repos/main.py
+++ remove_dir_from_all_repos/main.py
@@ -4,20 +4,21 @@ import subprocess
 
 # Path to the parent directory containing the subdirectories to search for DIRECTORY_TO_BE_DELETED
 PARENT_DIR = ""
-DIRECTORY_TO_BE_DELETED = ""
+DIRECTORY_TO_BE_DELETED_NAME = ""
 
 # Loop over all directories inside the parent directory
 for subdir in os.listdir(PARENT_DIR):
     subdir_path = os.path.join(PARENT_DIR, subdir)
     if os.path.isdir(subdir_path):
-        aws_account_dir = os.path.join(subdir_path, DIRECTORY_TO_BE_DELETED)
-        if os.path.isdir(aws_account_dir):
+        dir_to_be_deleted = os.path.join(
+            subdir_path, DIRECTORY_TO_BE_DELETED_NAME)
+        if os.path.isdir(dir_to_be_deleted):
             # Delete the "aws_account" directory
             try:
-                shutil.rmtree(aws_account_dir)
-                print(f"Deleted {aws_account_dir}")
+                shutil.rmtree(dir_to_be_deleted)
+                print(f"Deleted {dir_to_be_deleted}")
             except Exception as e:
-                print(f"Error deleting {aws_account_dir}: {e}")
+                print(f"Error deleting {dir_to_be_deleted}: {e}")
 
             # Push the changes to the GitHub repository
             commit_msg = "removed the aws_directory altogether to centralize the main.tf configuration"
@@ -36,4 +37,4 @@ for subdir in os.listdir(PARENT_DIR):
                 print(
                     f"Error pushing changes in {cleantrace_dir} to GitHub: {e}")
         else:
-            print(f"{aws_account_dir} does not exist in {subdir_path}")
+            print(f"{dir_to_be_deleted} does not exist in {subdir_path}")
